ef3bbebb7a5a33bacbbd3517360e1faca0a26f5d
azu-ks <kevin.seitz@mahlo.com>
2024-05-08 11:25:59 +0200

    Added a Method to read the configurations out of a json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import io.javalin.http.Context;
-
 public class Main {
     public static void main(String[] args) {
         /*Javalin app = Javalin.create().start(42069);
@@ -10,9 +8,8 @@ public class Main {
         app.get("/test", test::getAllProducts);
         app.get("/test/{special}", test::getSpecialProduct);*/
 
-        LDAPService test = new LDAPService();
-
-        test.login("azu-ks", "25K2005s");
+        Configurations c = new Configurations();
+        c.SQLConfig();
 
 
 
